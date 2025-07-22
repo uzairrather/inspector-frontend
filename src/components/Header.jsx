26cfc1b7628cad0abf4bc7bc78888bc2cc549e47
uzair rather <uzairrather3147@gmail.com>
2025-07-22 16:28:07 +0530

    last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -100,7 +100,7 @@ export default function Header() {
         </h1>
 
         {/* Center: Search Bar */}
-        <div className="absolute left-1/2 transform -translate-x-1/2">
+        {/* <div className="absolute left-1/2 transform -translate-x-1/2">
           <div className="relative w-80 sm:w-[22rem]">
             <input
               type="text"
@@ -112,7 +112,7 @@ export default function Header() {
             />
             <Search className="absolute left-3 top-3 w-5 h-5 text-gray-500" />
           </div>
-        </div>
+        </div> */}
 
         {/* Right: Language Toggle */}
         <Button
